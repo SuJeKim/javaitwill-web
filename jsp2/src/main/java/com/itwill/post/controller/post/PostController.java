16fb64dcc83be219b5dcd9ec1da92a0d2fa970bc
ITWILL <ITWILL@DESKTOP-DOAENUM>
2023-05-09 17:52:02 +0900

    servlet/JSP MVC 모델.

---
+++ jsp2/src/main/java/com/itwill/post/controller/post/PostController.java
@@ -0,0 +1,29 @@
+package com.itwill.post.controller.post;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class PostController
+ */
+@WebServlet(name = "postController", urlPatterns = { "/post" })
+// servlet을 만든 다음에 restart하기.
+public class PostController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("postController doGet() 호출");
+		
+		request.getRequestDispatcher("/WEB-INF/post/post.jsp").forward(request, response);
+	}
+
+
+}
---
+++ jsp2/src/main/webapp/WEB-INF/main.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" trimDirectiveWhitespaces="true"
+    %>
+<!DOCTYPE html>
+<html>
+	<head>
+	  	<meta charset="UTF-8" />
+		<title>POST</title>
+	</head>
+	<body>
+        <header>
+    		<h1>메인 페이지</h1>
+        </header>
+        
+        <!-- div대신 사용. -->
+        <nav>
+            <ul>
+                <li>
+                    <a href="post">포스트 목록 페이지</a> <!-- 상대경로 작성: context root(/post)까지 생각을 하고 그 이후 주소 부분 -->
+                </li>
+            </ul>
+        </nav>
+        
+	</body>
+</html>
