72868ffb0db174c93665db6a7ee4245664481bff
ITWILL <ITWILL@DESKTOP-DOAENUM>
2023-05-15 14:26:28 +0900

    filter

---
+++ jsp2/src/main/java/com/itwill/post/controller/user/UserSignInController.java
@@ -0,0 +1,43 @@
+package com.itwill.post.controller.user;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Servlet implementation class UserSignInController
+ */
+@WebServlet(name = "userSignInController", urlPatterns = { "/user/signin" })
+public class UserSignInController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static final Logger log = LoggerFactory.getLogger(UserSignInController.class);
+       
+    
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		log.info("doGet()");
+		
+		request.getRequestDispatcher("/WEB-INF/user/signin.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- jsp2/src/main/webapp/WEB-INF/main.jsp
+++ jsp2/src/main/webapp/WEB-INF/main.jsp
@@ -16,11 +16,21 @@
         <nav>
             <ul>
                 <li>
+                    <a>로그 아웃</a>
+                </li>
+                <li>
+                    <c:url var="signInPage" value="/user/signin"></c:url>
+                    <a href="${ signInPage }">로그인</a>
+                </li>
+                <li>
                     <c:url var="postlist" value="/post"></c:url> <!-- (서블릿) 해당 c:url에서만 /: 무조건 context root임. -->
                     <a href="${ postlist }">포스트 목록 페이지</a> <!-- 상대경로 작성: context root(/post)까지 생각을 하고 그 이후 주소 부분 -->
                 </li>
             </ul>
         </nav>
         
+        <main>
+            <!-- TODO -->
+        </main>
 	</body>
 </html>
