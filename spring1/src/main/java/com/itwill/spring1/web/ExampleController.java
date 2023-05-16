a81d5e678e2dff1d62e6c5ef6c1524c0afebd354
ITWILL <ITWILL@DESKTOP-DOAENUM>
2023-05-16 18:10:43 +0900

    Spring

--- spring1/src/main/java/com/itwill/spring1/web/ExampleController.java
+++ spring1/src/main/java/com/itwill/spring1/web/ExampleController.java
@@ -8,18 +8,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
+
+/*
+ * POJO(Plain Old Java Object): 평범한 자바 객체
+ * -특정 클래스를 상속해야만 하거나, 상속 후에 메서드들을 override해야만 하는 클래스가 아님.
+ * 
+ * 스프링 프레임워크는 POJO로 작성된 클래스를 controller로 사용할 수 있음. -> servlet을 상속받지 않아도 controller로 사용할 수 있음.
+ *  + 다른 과정은 dispatcherServlet이 다 함.
+ */
+
 @Slf4j // Logger 객체(log)를 생성.
-@Controller // Spring MVC 컴포넌트 애너테이션 중 하나. Controller 컴포넌트라는 것을 dispatcherServlet에게 알려줌.
+@Controller 
+// Spring MVC 컴포넌트 애너테이션(@Component, @Controller, @Service, @Repository, ...) 중 하나. 
+// Controller 컴포넌트라는 것을 dispatcherServlet에게 알려주는 역할
 public class ExampleController {
 
-    @GetMapping("/") // 해당 주소에 있는 메서드를 호출, 각 메서드마다 작성, 애너테이션을 사용함으로써 Dispatcher Servlet에서 찾을 수 있게 됨. 
+    
+    // 해당 애너테이션은 Handler Mapping.
+    @GetMapping("/") // GET 방식의 요청 주소가 "/"(Context root)인 요청을 처리하는 메서드.
+    // -> 추가 설명.
+    // 해당(요청) 주소를 처리하는 메서드를 호출 즉, 요청 매핑/ 각 메서드마다 작성, 
+    // 애너테이션을 사용함으로써 Dispatcher Servlet에서 찾을 수 있게 됨. 
     public String home(Model model) {
         log.info("home()"); // log가 오류가 나면 이클립스에서 lombok 라이브러리를 가지고 있어야 됨. + Help > about:에서 lombok이 보이면 해결됨(없으면 종료).
         
         LocalDateTime now = LocalDateTime.now();
         model.addAttribute("now", now);
         
-        return "index";
+        return "index"; // 매핑되어 있는 controller method가 리턴하는 것: 뷰의 이름.
     }
     
+    @GetMapping("/ex1") // context-root를 제외한 주소 작성.
+    public void example1() {
+        log.info("example() 호출");
+        // 컨트롤러 메서드가 void인 경우(뷰의 이름을 리턴하지 않는 경우)
+        // 요청 주소의 이름이 뷰의 이름이 됨.
+    }
+    // controller 메서드가 아무것도 리턴하지 않으면 파일 이름을 요청주소랑 똑같은 것을 찾음.(뷰를 찾음)  
+    /* -> Servlet-context.xml에서 다음과 같이 설정을 했기에 가능함.
+     * <bean class="org.springframework.web.servlet.view.InternalResourceViewResolver">
+        <property name="prefix" value="/WEB-INF/views/" /> <!-- 폴더 생성 + 리턴을 할 경우 리턴값이 value="/" 뒤에 붙음. 그래서 파일이름만 붙이면 됨.-->
+        <property name="suffix" value=".jsp" /> <!-- 해당 폴더의 파일의 확장자가 .jsp -->
+     </bean>   
+     */
 }
--- spring1/src/main/webapp/WEB-INF/views/index.jsp
+++ spring1/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8" trimDirectiveWhitespaces="true"
     %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>    
 <!DOCTYPE html>
 <html>
 	<head>
@@ -12,5 +13,14 @@
             <h1>메인 페이지</h1>
             <h2>${ now }</h2>
         </header>
+        
+        <nav>
+            <ul>
+                <li>
+                    <c:url var="ex1" value="/ex1" />
+                    <a href="${ ex1 }">Example 1</a>
+                </li> 
+            </ul>
+        </nav>
 	</body>
 </html>
